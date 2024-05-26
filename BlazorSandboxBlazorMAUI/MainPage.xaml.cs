aadc5ae7f9e0ea778abc85c120e76ac97842c8a9
Stacy Cashmore <33732891+StacyCash@users.noreply.github.com>
2024-05-26 11:04:12 +0200

    Add project - MAUI not working

---
+++ BlazorSandboxBlazorMAUI/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace BlazorSandboxBlazorMAUI
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
